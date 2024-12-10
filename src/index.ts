94337e4befc8c5d082d42cd0b216b84859a593b2
RutvikBhuvaa <rutvikbhuvaa9@gmail.com>
2024-12-10 13:33:39 +0530

    added client and write some code in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hi there");
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+async function createUser() {
+  await client.user.create({
+    data: {
+      username: "RutvikBhuvaa",
+      password: "123123",
+      age: 21,
+      city: "Bhavnagar",
+    },
+  });
+}
+
+createUser();
